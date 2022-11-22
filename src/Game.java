5363d7f0c7b3f8a15287d297a393ec1aca2ce9d3
makeitokay <andrey.mu11@yandex.ru>
2022-11-23 01:24:46 +0300

    set start field!

--- src/Game.java
+++ src/Game.java
@@ -3,6 +3,11 @@ public class Game {
 
     public Game() {
         field = new Field(Constants.FIELD_SIZE);
+
+        field.setChip(3, 3, ChipType.White);
+        field.setChip(3, 4, ChipType.Black);
+        field.setChip(4, 3, ChipType.Black);
+        field.setChip(4, 4, ChipType.White);
     }
 
     public Field getField() {
